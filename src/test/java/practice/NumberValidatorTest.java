cf6fb2179dbe04926849a5cef541616c592fb933
JACK Z X HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGJA14@OOCL.COM>
2020-07-23 21:46:15 +0800

    [GuassNumberGame][Jack] test: should_return_true_when_check_number_given_1_3_2_4

--- src/test/java/practice/NumberValidatorTest.java
+++ src/test/java/practice/NumberValidatorTest.java
@@ -49,4 +49,15 @@ public class NumberValidatorTest {
 //        then
         assertEquals(false, result);
     }
+
+    @Test
+    public void should_return_true_when_check_number_given_1_3_2_4(){
+//        given
+        NumberValidator numberValidator = new NumberValidator();
+        int[] number = {1,3,2,4};
+//        when
+        boolean result = numberValidator.checkNumber(number);
+//        then
+        assertEquals(true, result);
+    }
 }
