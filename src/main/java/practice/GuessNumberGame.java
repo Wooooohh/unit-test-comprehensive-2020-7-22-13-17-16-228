2003efa6871bce2777a016ca88a891d2b38ccf0f
JACK Z X HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGJA14@OOCL.COM>
2020-07-23 20:06:59 +0800

    [GuassNumberGame][Jack] feature: pass should_return_4A0B_when_generateResult_given_num_all_right

--- src/main/java/practice/GuessNumberGame.java
+++ src/main/java/practice/GuessNumberGame.java
@@ -1,4 +1,11 @@
 package practice;
 
 public class GuessNumberGame {
+    public String generateResult(int[] number, int[] answer) {
+        for(int i = 0; i <4; i++){
+            if(number[i] != answer[i])
+                return null;
+        }
+        return "4A0B";
+    }
 }
