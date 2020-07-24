c01a7498f0653a76c0008667cf503082de5cc227
JACK Z X HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGJA14@OOCL.COM>
2020-07-24 08:44:49 +0800

    [GuassNumberGame][Jack] feature: pass should_return_false_when_check_number_given_1_2_2_4

--- src/main/java/practice/NumberValidator.java
+++ src/main/java/practice/NumberValidator.java
@@ -3,12 +3,19 @@ package practice;
 public class NumberValidator {
 
     boolean checkNumber(int[] number){
+        int[] cache = new int[4];
         if(number.length != 4)
             return false;
-        for(int i = 0; i < number.length; i++){
+        for(int i = 0; i < 4; i++){
             if(number[i] < 0 || number[i] > 9){
                 return false;
             }
+        }
+        for(int i = 0; i < 4; i++){
+            if(cache[number[i]] !=1)
+                cache[number[i]] = 1;
+            else
+                return false;
         }
         return true;
     }
