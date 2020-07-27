ca36ead77bf087b2de8c4533fdc1c141acbb1103
JACK Z X HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGJA14@OOCL.COM>
2020-07-27 20:31:21 +0800

    [process controller] [Jack] feature: pass should_return_1_when_given_isExit_given_process_controller_1A1B_and_times_is_6

--- src/main/java/practice/ProcessController.java
+++ src/main/java/practice/ProcessController.java
@@ -3,9 +3,12 @@ package practice;
 public class ProcessController {
 
     public int isExit(String result, int times) {
-        if (times > 0 && times < 6)
+        if (times > 0 && times < 6) {
             if (result.equals("4A0B"))
                 return 0;
+        }
+        if(times == 6)
+            return 1;
         return -1;
     }
 
