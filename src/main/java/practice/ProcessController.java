c017e649c5236edb0c54ef2bea19b027cecbee18
JACK Z X HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGJA14@OOCL.COM>
2020-07-27 22:13:33 +0800

    [ProcessController] [Jack] refact: extract judgment function from isExit

--- src/main/java/practice/ProcessController.java
+++ src/main/java/practice/ProcessController.java
@@ -5,13 +5,21 @@ import static practice.ConstantPool.WIN_RESULT;
 public class ProcessController {
 
     public int isExit(String result, int times) {
-        if (times > 0 && times < 6) {
+        if (isTimesInRange(times)) {
             if (result.equals(WIN_RESULT))
                 return Status.WIN.getCode();
         }
-        if(times == 6)
+        if (isTimesOver(times))
             return Status.RUN_OUT.getCode();
         return Status.CONTINUE.getCode();
+    }
+
+    private boolean isTimesOver(int times) {
+        return times == 6;
+    }
+
+    private boolean isTimesInRange(int times) {
+        return times > 0 && times < 6;
     }
 
 }
